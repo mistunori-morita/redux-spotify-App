0ebc0aff3fe9a4faa06565e08527e7733919c277
mitsunori-morita <mitsunori.morita@speee.jp>
2018-07-07 12:24:34 +0900

    Add style component UI part1

--- README.md
+++ README.md
@@ -15,3 +15,9 @@ $ npm start
   - package.jsonに記載されているバージョンに更新
 
 - 一応最後に`npm audit`をやる
+
+### fontawesome
+- `<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.1.0/css/all.css" integrity="sha384-lKuwvrZot6UHsBSfcMvOkWwlCMgc0TaWr+30HWe3a4ltaBwTZhyTEggF5tJv8tbt" crossorigin="anonymous">`
+
+### materialize-cssでUI構築
+- `yarn add materialize-css`でUI設定
--- src/views/index/index.js
+++ src/views/index/index.js
@@ -1,10 +1,35 @@
 import React, { Component } from 'react'
+import './index.css'
+import 'materialize-css/dist/css/materialize.min.css'
 
 class Index extends Component{
+  constructor(){
+    super();
+    this.state ={
+      song: ''
+    }
+  }
   render(){
     return(
-      <div>
-        Index
+      <div className="Index">
+        <div className="card">
+          <div className="card-content">
+            <div className="Index-serachBox">
+              <input type="text" 
+              className="Index-serachBox-input"
+              placeholder="Search"
+              onChange={(e)=>{ this.setState({ song: e.target.value})}}
+              value={this.state.song}
+              />
+              <a href="" className="waves-effect waves-light btn green">
+              <i className="fa fa-search"></i>
+              </a>
+            </div>
+            <div>
+              {/* todo*/}
+            </div>
+          </div>
+        </div>
       </div>
     )
   }
